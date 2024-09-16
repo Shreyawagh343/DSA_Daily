f973d1fbc03b9aa5f23e9daf5d26c14aa2c7e6ca
shreyawagh343 <shreyawagh343@gmail.com>
2024-09-16 14:37:18 +0530

    day-2

--- day-2/day2.cpp
+++ day-2/day2.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout<<"shreya";
+	
+// variable and data type 
+
+//	int age = 20;
+//	char grade = 's';
+//	string name = 'shreya';
+//	cout<<age<<endl;
+//	cout<<grade<<endl;
+//	cout<<name<<endl;
+
+// taking inputs
+
+//int age ;
+//cout<<"Enter your age : ";
+//cin>>age;
+//cout<<"Your age is:"<<endl;
+
+// Operators 
+
+//int age =30;
+//int price =40;
+//
+//cout<<(3>=4)<<endl;
+
+	return 0 ;
 }
