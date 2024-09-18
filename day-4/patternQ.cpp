e47ef6588feccaebd0f50f4886173032fc10234a
shreyawagh343 <shreyawagh343@gmail.com>
2024-09-18 13:30:24 +0530

    day-4

---
+++ day-4/patternQ.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+// pattern Question
+// 1. numbers
+//	int n =3;
+//	for(int i=1;i<=n;i++){
+//		for(int j=1;j<=n;j++){
+//			cout<<j;
+//		}
+//	cout<<endl;
+//	}
+
+// 2.charater
+//int n=26;
+//for(int i=1;i<=n;i++){
+//	char ch='A';
+//	for(int j=0;j<n;j++){
+//		cout<<ch;
+//		ch=ch+1;
+//	}
+//	cout<<endl;
+
+
+// 3. different number 123 456 789
+
+//int n=3;
+//	int num = 1;
+//for(int i=0;i<n;i++){
+//	for(int j=0;j<n;j++){
+//		cout<<num;
+//		num++;
+//	}
+//	cout<<endl;
+//}
+
+// 4. triangle star
+
+//int n=4;     
+//for(int i=0;i<n;i++){
+//	for(int j=0;j<i+1;j++){
+//	cout<<"*";
+//	}
+//	cout<<endl;
+//}
+
+// 5. traiangle in numbers 1 22 333 4444
+
+//int n =4;
+//for(int i=0;i<n;i++){
+//	for(int j=0;j<i+1;j++){
+//		cout<<(i+1);
+//	}
+//	cout<<endl;
+//}
+
+// 6.triangle for 1 12 123 1234
+
+//int n =4;
+//for(int i=0;i<n;i++){
+//	for(int j=0;j<i+1;j++){
+//		cout<<j+1;
+//	}
+//	cout<<endl;
+//}
+
+// 7. backward loop 
+
+//int n =4;
+//for(int i=0;i<n;i++){
+//	for(int j=i+1;j>0;j--){
+//		cout<<j;
+//	}
+//	cout<<endl;
+//}
+
+return 0;
+}
+
