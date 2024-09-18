b5dd86e79b9ef72ae281d505521f367eb3603b6d
shreyawagh343 <shreyawagh343@gmail.com>
2024-09-18 11:40:53 +0530

    day-4

---
+++ day-4/day4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Nested loop
+//	int X;
+//	cout<<"Enter no. of X:"<<endl;
+//	cin>>X;
+//	
+//	for(int i=1;i<=X;i++){
+//		cout<<"*";
+//	}
+//	
+
+// sum of all number divisible by 3
+	int X;
+	int sum;
+	cout<<"Enter no. of X:"<<endl;
+	cin>>X;
+	
+	for(int i=0;i<=X;i++){
+		if(i%3==0){
+			sum=sum+i;
+		}
+	
+	}
+		cout<<sum<<endl;
+
+}
