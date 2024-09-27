fa43bbfe159cac6521033ece19e92de858a25171
shreyawagh343 <shreyawagh343@gmail.com>
2024-09-27 12:09:16 +0530

    day-8

---
+++ day-8/arrayPractise.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+	// largest number
+//	int arr[]={12,34,56,78,4};
+//	int largest=0;
+//	largestElement=0;
+//	int size=5;
+//	
+//	for(int i=0;i<size;i++){
+//		if(arr[i]>largest){
+//			largest=arr[i];
+//			}
+//	}
+//	cout<<largest<<endl;
+
+// second large element 
+
+int 
+
+
+
+}
