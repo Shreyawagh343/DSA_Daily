42d585239d085116287c166b0238d9c62abad2fc
shreyawagh343 <shreyawagh343@gmail.com>
2024-09-30 20:41:21 +0530

    day-11

---
+++ day-11/bitwise.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+// BITWISE OPERATOR
+//&
+int a=3 , b=4;
+   cout<<(a & b)<<endl;	
+   //|
+   cout<<(a | b)<<endl;
+   //^
+   cout<<(a ^ b)<<endl;
+   // left shift <<
+   cout<<(4 << 1)<<endl;
+   //Right shift >>
+   cout<<(4 >> 1)<<endl;
+   
+    
+	return 0;
+}
